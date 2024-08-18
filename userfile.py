c303701388b8aa214bd80dfa3ed3355df6d443a6
Chandrashekar Babu <chandra@slashprog.com>
2024-08-18 13:58:25 +0530

    Undo temperary changes to

--- userfile.py
+++ userfile.py
@@ -1,5 +1,6 @@
 class UserAlreadyExists(Exception): pass
 class UserNotFound(Exception): pass
+class MissingPasswordOrFullname(Exception): pass
 
 class Users:
     def __init__(self, filename, sep=","):
@@ -8,13 +9,13 @@ class Users:
         self._users = {}
 
     def _parse(self):
-        self._users.clear()
+        #self._users.clear()
         with open(self.filename) as u:
             for line in u:
                 line = line.strip()
                 if not line:  # if not bool(line)
                     continue
-                u, p, f = line.split(sep)
+                u, p, f = line.split(self.sep)
                 rec = {"username": u, "password": p, "fullname": f}
                 self._users[u] = rec
 
@@ -23,7 +24,7 @@ class Users:
             lines = "\n".join( # Join each row with a newline
                        self.sep.join(rec.values()) # John each column using sep
                        for rec in self._users.values()) # For each row in data
-        out.write(lines)
+            out.write(lines)
         self._users.clear()
 
     def add(self, username, password, fullname):
@@ -38,7 +39,29 @@ class Users:
 
     def modify(self, username, password=None, fullname=None):
         self._parse()
-        # TODO
+        rec = {}
+        if username not in self._users:
+            raise UserNotFound(username)
+        
+        if password is None and fullname is None:
+            raise MissingPasswordOrFullname("either password / fullname must be passed")
+
+        if password is not None:
+            if type(password) is str:
+                rec["password"] = password
+                # self._users[username]["password"] = password
+            else:
+                raise TypeError("password must be a valid string")
+        
+        if fullname is not None:
+            if type(fullname) is str:
+                rec["fullname"] = fullname
+                # self._users[username]["fullname"] = fullname
+            else:
+                raise TypeError("fullname must be a valid string")
+        
+        self._users[username].update(rec)
+
         self._store()
 
     def delete(self, username):
@@ -54,3 +77,10 @@ if __name__ == '__main__':
         users.add("john", password="john123", fullname="John Doe")
     except UserAlreadyExists as e:
         print("Failed to add user john already exists")
+
+    #users.modify("john", password="welcome")
+    #users.modify("john", fullname="John Adams")
+    #users.modify("john", password="secret", fullname="John Doe")
+
+    #users.modify("john") # raise Error
+    #users.modify("john", role="Developer") # raise Error
